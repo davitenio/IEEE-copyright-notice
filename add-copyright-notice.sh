b76292abf4eb45bacbf3b3393d8c465cc81401b2
David Gessner <davidges@gmail.com>
2014-07-20 19:40:33 +0100

    Do not call evince from script

--- add-copyright-notice.sh
+++ add-copyright-notice.sh
@@ -27,5 +27,3 @@ pdflatex copyMarkTmp.tex
 pdftk $FILE cat 1 output - | pdftk - stamp copyMarkTmp.pdf output - | pdftk A=- B=$FILE cat A1 B2-end output ${y}_copy.pdf
 
 rm copyMarkTmp.tex
-
-evince ${y}_copy.pdf&
