21093baf681b061119a0a4bf9f13dc1b13ee78e2
David Gessner <davidges@gmail.com>
2014-07-20 19:21:14 +0100

    Add script and template

---
+++ add-copyright-notice.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILE=$1
+YEAR=$2
+DOI=$3
+
+sed -e "s/YEAR/$YEAR/" -e "s#DOI#$DOI#" copyMark.tex > copyMarkTmp.tex
+
+y=${FILE%.pdf}
+
+pdflatex copyMarkTmp.tex
+
+pdftk $FILE cat 1 output - | pdftk - stamp copyMarkTmp.pdf output - | pdftk A=- B=$FILE cat A1 B2-end output ${y}_copy.pdf
+
+rm copyMarkTmp.tex
+
+evince ${y}_copy.pdf&
